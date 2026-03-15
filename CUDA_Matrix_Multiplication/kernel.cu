94ea93b836a91060a452d47e8aaa3126c0c4a3a4
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2026-03-15 19:51:34 +0530

    Add project files.

---
+++ CUDA_Matrix_Multiplication/kernel.cu
@@ -0,0 +1,12 @@
+﻿
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+int main()
+{
+	int m, n;
+	printf("Enter the number of rows and columns of the matrix: ");
+	scanf("%d %d", &m, &n);
+}
